8093d20e93cb73d10bd0b5bd48600f545819c8b8
RScribano <r.scribano@campus.unimib.it>
2025-04-17 20:49:20 +0200

    wrapped up gene signature analysis

--- analysis preprocessed Joli merged carts.R
+++ analysis preprocessed Joli merged carts.R
@@ -1,12 +1,19 @@
 # packages ####
+#tidyverse
 library(dplyr)
 library(tidyr)
 library(purrr)
-library(ggplot2)
+
+#single cell analysis
 library(scran)
 library(scater) 
+
+#plotting
+library(ggplot2)
+library(ggridges)
 library(scales)
 library(patchwork)
+library(UCell)
 
 # import data ####
 
@@ -261,14 +268,212 @@ for (gene in gene_signature_detected){
 plots <- purrr::map(gene_signature_detected, function(gene_name){
   umap_df |>
     filter(sample_type != "Healthy") |>
-    ggplot(aes(x = UMAP1, y=  UMAP2, color = .data[[gene_name]]))+
-    geom_point(size = 0.6, alpha = 0.8)+
+    ggplot(aes(x = UMAP1, y=  UMAP2, color = .data[[gene_name]], alpha = .data[[gene_name]]))+
+    geom_point(size = 0.6)+
     scale_color_distiller(palette = "Spectral")+
     ggtitle(gene_name)+
     theme_minimal(base_size = 18)
 })
 grid<-wrap_plots(plots, ncol = 6, axes = "collect")
-ggsave("20250415_gene signature patchwork.png",
+ggsave("20250415_gene signature patchwork_1.png",
        plot = grid, width = 2100,height =1226, unit = "px",
        scale = 5)
+
+
+# two layers
+plots <- purrr::map(gene_signature_detected, function(gene_name) {
+  umap_df |>
+    filter(sample_type != "Healthy") |>
+    ggplot(aes(x = UMAP1, y = UMAP2)) +
+    
+    # Layer 1: all points, grey
+    geom_point(color = "grey90", size = 0.6) +
+    
+    # Layer 2: only gene > 0, with color + alpha
+    geom_point(
+      
+      aes(color = .data[[gene_name]]),
+      size = 0.6
+    ) +
+    
+    scale_color_distiller(palette = "Blues", direction = 1) +  # or use scale_color_steps()
+    guides(alpha = "none") +  # Hide alpha legend if too noisy
+    ggtitle(gene_name) +
+    theme_minimal(base_size = 18)
+})
+
+grid<-wrap_plots(plots, ncol = 6, axes = "collect")
+ggsave("20250415_gene signature patchwork_3.png",
+       plot = grid, width = 2100,height =1226, unit = "px",
+       scale = 5)
+
+# Cell signature scoring using UCell ####
+signature <- list(
+  metabolic_genes= c("CYCS","CYC1", "CS","NDUFA4","NDUFC1","NDUFS2","NDUFS5","NDUFV1","NDUFB3", 
+                      "NDUFA13","NDUFB8","NDUFB10","NDUFA3","NDUFB9","NDUFB11","NDUFS1","NDUFA11","NDUFS8", 
+                      "NDUFA2","NDUFA9","MRPL3","MRPS21","MRPL17","MRPS30","MRPL16")
+)
+sce <- ScoreSignatures_UCell(sce, features = signature,
+                             assay = 'logcounts', name = NULL)
+
+# export as dataframe column
+
+umap_df$UCell_signature_score <- t(as.data.frame(assay(altExp(sce, "UCell"))))
+
+# plottini..
+
+ggplot(data = umap_df, aes(x= UMAP1, y=UMAP2))+
+  geom_point(size = 0.7, aes(color = UCell_signature_score))+
+  scale_color_gradient(low = "grey70", high = "purple")+
+  theme_minimal(base_size = 22)+
+  facet_grid(~sample_type, cols = 2, rows = NULL)
+
+#barplot cell signature score in B-cells only
+
+umap_df$Cell_Type_Experimental <- colData(sce)$Cell_Type_Experimental
+
+umap_df |> 
+  group_by(sample_type, Cell_Type_Experimental)|>
+  summarise(mean_UCell_score = 
+              mean(UCell_signature_score),
+            se = sd(UCell_signature_score)/sqrt(n()),
+            .groups = "drop") |>
+  ggplot(aes(x = Cell_Type_Experimental, y =mean_UCell_score ))+
+  geom_bar(aes(fill = Cell_Type_Experimental), stat = "identity", position =position_dodge())+
+  geom_errorbar(aes(ymin = mean_UCell_score - se, 
+                    ymax = mean_UCell_score + se),
+                position = position_dodge(),
+                width = 0.2)+
+  scale_fill_brewer(palette = "Set3")+
+  facet_wrap(~sample_type)+
+  theme_minimal(base_size = 22)+
+  theme( axis.text.x = element_blank())
+ 
+
+# working on ADT expression ####
+
+#fixing names 
+original_tags <- rownames(altExp(sce, "ADT"))
+
+new_tags <- original_tags |>
+  sub("-.*", "", x = _) |>
+  sub(":.*", "", x= _)
+  
+rowData(altExp(sce, "ADT"))$antigen <- new_tags
+
+rownames(altExp(sce, "ADT")) <- rowData(altExp(sce, "ADT"))$antigen
+
+# extract adt expression as dataframe
+adt_df <- as.data.frame(t(logcounts(altExp(sce, "ADT"))))
+
+# add key for sce metadata 
+adt_df$Sample_Name <- coldata_df$Sample_Name
+
+#export just in case
+write.csv(adt_df, "20250415_adt_exp.csv")
+
+# plot distributions ADT #### 
+
+# merge with metadata (we didn't save it already merged to spare memory)
+
+adt_df$UMAP1 <- umap_df$UMAP1
+adt_df$UMAP2 <- umap_df$UMAP2
+adt_df$individual <- colData(sce)$individual
+adt_df$timepoint <- colData(sce)$timepoint
+adt_df$sample_type <- colData(sce)$sample_type
+adt_df$relapse <- colData(sce)$relapse
+adt_df$MRD_risk <- colData(sce)$MRD_risk
+
+
+
+# plot distirbutions by sample type
+
+
+#pivot to long format
+adt_long <- adt_df |>
+  tidyr::pivot_longer(
+    cols = -c("individual", "timepoint","sample_type", "relapse", "MRD_risk", "Sample_Name"),
+    names_to = "marker",
+    values_to = "logcount"
+    )
+
+# joyplot/ridges
+  ggplot(adt_long, aes(x = logcount, y= marker, fill = marker))+
+    geom_density_ridges(panel_scaling = TRUE)+
+    facet_grid(~sample_type)
+  
+  
+  # joyplot/ridges
+  
+  adt_long |>
+    filter(sample_type != "Healthy")|>
+  ggplot(aes(x = logcount, y= marker, fill = marker))+
+    geom_density_ridges(panel_scaling = TRUE)+
+    theme_minimal(base_size = 24)+
+    labs(rota)
+    facet_grid(~timepoint)
+
+
+# UMAP colored by ADT ####
+
+
+# plot umaps of all patients (not healthy)
+plots <- purrr::map(colnames(adt_df[1:15]), function(adt) {
+  adt_df |>
+    filter(sample_type != "Healthy") |>
+    ggplot(aes(x = UMAP1, y = UMAP2)) +
+             
+             # first layer grey
+    geom_point(size = 0.6, color = "grey80", alpha = 0.5) +
+      
+    geom_point(size = 0.6,
+               aes(color = .data[[adt]], alpha = .data[[adt]]))+
+    scale_color_distiller(palette = "Reds", direction = 1) +
+    guides(alpha = "none") + 
+    ggtitle(adt) +
+    theme_minimal(base_size = 18)
+    })
+
+grid<-wrap_plots(plots, ncol = 6, axes = "collect")
+ggsave("20250415_umap_adt_1.png",
+       plot = grid, width = 2100,height =1226, unit = "px",
+       scale = 5)
+
+# plot for each gene divded by sample type ####
+plots <- purrr::map(gene_signature_detected, function(gene_name){
+  umap_df |>
+    ggplot(aes(x = UMAP1, y=  UMAP2, color = .data[[gene_name]], alpha = .data[[gene_name]]))+
+    geom_point(size = 0.6)+
+    scale_color_gradient(low= "grey70", high = "blue")+
+    guides(alpha = "none")+
+    ggtitle(gene_name)+
+    facet_wrap(~sample_type, ncol = 4)+
+    theme_minimal(base_size = 20)
+})
+
+purrr::walk2(
+  .x = plots,
+  .y = gene_signature_detected,
+  .f = ~ggsave(
+    filename = paste0("20250416_", .y, ".png"),
+    plot = .x,
+    width = 1100,
+    height = 680,
+    unit = "px",
+    dpi = 72,
+    scale = 1,
+    limitsize = FALSE,
+    bg = "white"
+  )
+)
+
+
+ggplot(data = umap_df,aes(x = UMAP1, y=  UMAP2))+
+  geom_point(size = 0.6, aes(color = CYCS, alpha = CYCS))+
+  scale_color_gradient(low = "grey70", high = "blue")+
+  guides(alpha = "none")+
+  ggtitle("CYCS")+
+  facet_wrap(~ sample_type, ncol = 4)+
+  theme_minimal(base_size = 18)
+
 
